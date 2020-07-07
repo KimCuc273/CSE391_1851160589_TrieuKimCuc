fed6037190bc177ba0a0a901a1e167fd01145ac0
cucxg <1851160589@e.tlu.edu.vn>
2020-07-08 00:21:45 +0700

    Add hw

---
+++ homework_01/javascripclass.js
@@ -0,0 +1,13 @@
+// console.log("Hello world");
+// alert("Tôi là alert")
+// var toilaBien;
+// function tenham{
+//     var bien=7;//biến cục bộ
+// }
+    
+
+// console.log(bien)
+
+function SUM(a,b){
+    return (a+b);
+}

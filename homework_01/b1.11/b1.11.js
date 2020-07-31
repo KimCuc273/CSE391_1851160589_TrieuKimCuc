13594c2264cb469cc6a14ba2f0d678c7eedf22a9
cucxg <1851160589@e.tlu.edu.vn>
2020-07-31 10:32:17 +0700

    modified HW1

---
+++ homework_01/b1.11/b1.11.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+            
+            
+    $("button").click(function () { 
+        var str = $("#myInput").val();
+        $("#maN").text(str);
+    });
+});
---
+++ homework_01/b1.12/b1.12.js
@@ -0,0 +1,4 @@
+$("button").click(function () { 
+    $("#tb").text("Bạn đã đăng ký thành công!!!");
+    
+});
